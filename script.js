26525497d7968938dcc4716464c0033577b37ff4
Elliot <ellthrowaway2@icloud.com>
2024-05-21 13:44:22 -0400

    cleaned up script; styling; added input placeholder text

--- script.js
+++ script.js
@@ -1,43 +1,31 @@
-
-/* VARIABLES */
 const textInput = document.getElementById("text-input")
 const checkBtn = document.getElementById("check-btn")
 const result = document.getElementById("result")
 const palindromeDefinition = document.getElementById("palindrome-definition")
 
-
-
-
-/* FUNCTIONS */
-
-
-// submit input
 const submit = () => {
     let str = textInput.value
     let cleanStr = [];
-    // check if input is empty
     if (!str) {
-        alert('Please input a value') // √
+        alert('Please input a value')
     } else {
-        // remove non-alphanum chars √
         const removeInvalidChars = str => {
             const regex = /[^a-z0-9]/ig
             cleanStr = str.replace(regex, "")
             return cleanStr
         }
-        removeInvalidChars(str) // √
-        console.log(removeInvalidChars(str)) // √
-        // check for palindrome
-        let strArr = cleanStr.split("") // √
+        removeInvalidChars(str)
+        console.log(removeInvalidChars(str))
+        let strArr = cleanStr.split("")
         console.log(strArr)
-        let palindrome = strArr.reverse("").join("") // √
+        let palindrome = strArr.reverse("").join("")
         console.log(`Palindrome is: ${palindrome.toLowerCase()}`)
         const isPalindrome = (str1, str2) => {
             if (str1.toLowerCase() !== str2.toLowerCase()) {
-                result.innerText = `${str} is not a palindrome`
+                result.innerHTML = `<span><strong>${str}</strong></span> is not a palindrome`
                 return console.log(`${str} is not a palindrome`)
             } else {
-                result.innerText = `${str} is a palindrome`
+                result.innerHTML = `<span><strong>${str}</strong></span> is a palindrome`
                 return console.log(`${str} is a palindrome`)
             }
         }
@@ -45,10 +33,5 @@ const submit = () => {
     }
     textInput.value = ""
 }
-
-
-
-
-/* EVENT LISTENERS */
 
 checkBtn.addEventListener('click', submit)
