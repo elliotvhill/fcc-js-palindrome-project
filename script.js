230f1bcf727dc907ecfd0f1cf44daa1bb6ac9b4e
Elliot <ellthrowaway2@icloud.com>
2024-05-21 13:15:04 -0400

    fixed regex max to exclude underscore; all fCC use cases pass local testing but still not accepted by IDE

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const palindromeDefinition = document.getElementById("palindrome-definition")
 
 // submit input
 const submit = () => {
-    let str = textInput.value.toLowerCase()
+    let str = textInput.value
     let cleanStr = [];
     // check if input is empty
     if (!str) {
@@ -21,7 +21,7 @@ const submit = () => {
     } else {
         // remove non-alphanum chars √
         const removeInvalidChars = str => {
-            const regex = /\W+/g
+            const regex = /[^a-z0-9]/ig
             cleanStr = str.replace(regex, "")
             return cleanStr
         }
@@ -31,12 +31,12 @@ const submit = () => {
         let strArr = cleanStr.split("") // √
         console.log(strArr)
         let palindrome = strArr.reverse("").join("") // √
-        console.log(`Palindrome is: ${palindrome}`)
+        console.log(`Palindrome is: ${palindrome.toLowerCase()}`)
         const isPalindrome = (str1, str2) => {
-            if (str1 !== str2) {
-                return console.log(`${str1} is not a palindrome`)
+            if (str1.toLowerCase() !== str2.toLowerCase()) {
+                return console.log(`${str} is not a palindrome`)
             } else {
-                return console.log(`${str1} is a palindrome`)
+                return console.log(`${str} is a palindrome`)
             }
         }
         isPalindrome(cleanStr, palindrome)
